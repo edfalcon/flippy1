8758bb43026363df03ebe92ed43d390bf2b6e7d7
Ed Falcon <ed@thefalcons.xyz>
2024-05-09 21:40:51 -0500

    Initial Commit

---
+++ flippy1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  flippy1
+//
+//  Created by Edward Falcon on 5/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ flippy1/flippy1App.swift
@@ -0,0 +1,17 @@
+//
+//  flippy1App.swift
+//  flippy1
+//
+//  Created by Edward Falcon on 5/9/24.
+//
+
+import SwiftUI
+
+@main
+struct flippy1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
